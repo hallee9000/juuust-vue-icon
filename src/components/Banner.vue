a4c8d5acf15225049ddd9b70c3da5bc8fb3ec03f
Tuxling 图思灵 <72972692+infotuxling@users.noreply.github.com>
2020-10-19 12:37:15 +0800

    Update Banner.vue

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <h1 class="banner">Juuust Vue Icon</h1>
+    <h1 class="banner">Tuxling Vue Icon</h1>
     <div class="code">
       <div>
         <h3>Installation</h3>
-        <pre><code>npm install juuust-vue-icon --save<br/>yarn add juuust-vue-icon</code></pre>
+        <pre><code>npm install tuxling-vue-icon --save<br/>yarn add tuxling-vue-icon</code></pre>
       </div>
       <div>
         <h3>Usage</h3>
-        <pre><code>import { IconHome } from 'juuust-vue-icon'<br/>&lt;IconHome :size="36" color="#009C22" &gt;</code></pre>
+        <pre><code>import { IconHome } from 'tuxling-vue-icon'<br/>&lt;IconHome :size="36" color="#009C22" &gt;</code></pre>
       </div>
     </div>
   </div>
