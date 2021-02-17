3d5226886edbff14ff57fecd4e6673a73b30e581
Tim Rutten <tim@3fiftynine.nl>
2021-02-17 16:47:55 +0100

    added first set of custom icons in vue

---
+++ src/icons/arrow-right.vue
@@ -0,0 +1,23 @@
+
+  <template>
+    <svg
+      xmlns="http://www.w3.org/2000/svg" :width="size" :height="size" viewBox="0 0 18 14" aria-hidden="true" v-on="$listeners"
+    >
+      <path d="M1 7H17M17 7L11 1M17 7L11 13" :stroke="color" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  </template>
+  <script>
+    export default {
+      name: "IconArrowRight",
+      props: {
+        size: {
+          type: Number,
+          default: 16
+        },
+        color: {
+          type: String,
+          default: "currentColor"
+        }
+      }
+    };
+  </script>
