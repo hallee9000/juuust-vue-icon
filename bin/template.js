3ed6b2078d29a8d95217dfa08a647c069aa3b124
Tim Rutten <tim@3fiftynine.nl>
2021-02-22 09:57:14 +0100

    added gradiant to every icon

--- bin/template.js
+++ bin/template.js
@@ -26,6 +26,11 @@ const getElementCode = (ComponentName, attrs, svgCode) => `
       ${attrs}
     >
       ${svgCode}
+      <defs>
+        <linearGradient id="primary-gradiant" x1="0.542969" y1="60.582" x2="60.543" y2="0.582212" gradientUnits="userSpaceOnUse">
+          <stop stop-color="var(--color-stop-1)" /><stop offset="1" stop-color="var(--color-stop-2)" />
+        </linearGradient>
+      </defs>
     </svg>
   </template>
   <script>
